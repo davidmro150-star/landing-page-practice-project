69bdda800bec1d05a0665ba17842e9222908474a
davidmro150-star <davidmro150@gmail.com>
2026-05-08 21:48:29 +0600

    Completed Business section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Experience from './assets/Components/Experience/Experience'
 import Service from './assets/Components/Service/Service'
 import Latest from './assets/Components/Latest/Latest'
 import Project from './assets/Components/Project/Project'
+import Business from './assets/Components/Business/Business'
 
 
 const App = () => {
@@ -16,7 +17,8 @@ const App = () => {
         <Experience />
         <Service />
         <Latest />
-        <Project/>
+        <Project />
+      <Business/>
       </div>
       
     </>
